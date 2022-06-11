6610a23b4fc7d813a3c422e37a8853440fd81676
yoshitsugu <yoshitsugu@users.noreply.github.com>
2022-06-11 21:08:08 +0900

    Change failure to thiserror

--- src/error.rs
+++ src/error.rs
@@ -1,39 +1,15 @@
-use failure::Fail;
+use thiserror::Error;
 use rustc_serialize::json;
 use std::io;
 
-#[derive(Debug, Fail)]
+#[derive(Debug, Error)]
 pub enum SearchError {
-    #[fail(display = "{}", _0)]
-    Io(#[fail(cause)] io::Error),
-    #[fail(display = "{}", _0)]
-    Json(#[fail(cause)] json::BuilderError),
-    #[fail(display = "{}", _0)]
+    #[error("{0}")]
+    Io(#[from] io::Error),
+    #[error("{0}")]
+    Json(#[from] json::BuilderError),
+    #[error("{0}")]
     Msg(&'static str),
-    #[fail(display = "{}", _0)]
-    Request(#[fail(cause)] reqwest::Error),
-}
-
-impl From<io::Error> for SearchError {
-    fn from(err: io::Error) -> SearchError {
-        SearchError::Io(err)
-    }
-}
-
-impl From<json::BuilderError> for SearchError {
-    fn from(err: json::BuilderError) -> SearchError {
-        SearchError::Json(err)
-    }
-}
-
-impl From<&'static str> for SearchError {
-    fn from(err: &'static str) -> SearchError {
-        SearchError::Msg(err)
-    }
-}
-
-impl From<reqwest::Error> for SearchError {
-    fn from(err: reqwest::Error) -> SearchError {
-        SearchError::Request(err)
-    }
+    #[error("{0}")]
+    Request(#[from] reqwest::Error),
 }
